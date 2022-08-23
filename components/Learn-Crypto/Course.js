2c98e26b30f63d7b2e112d13335f21d7e24d65b9
Tony Stark <tony_stark_dex@protonmail.com>
2022-08-23 05:22:00 -0500

    merged bankless academy update

--- components/Learn-Crypto/Course.js
+++ components/Learn-Crypto/Course.js
@@ -8,7 +8,7 @@ function Course() {
       <h2 className="text-3xl font-bold py-4">Learn Crypto</h2>
       {/* <a href="https://app.banklessacademy.com/" target='_blank'>
         <Card title="Bankless Academy" description="Bankless Academy is on a mission to introduce 1 billion people to the exciting possibilities of cryptocurrency, DeFi, and beyond." url={card2} /></a> */}
-      <iframe src="https://app.banklessacademy.com/lessons/wallet-basics?embed=bankless-africa" width="100%" height="850px" frameborder="0"></iframe>
+      <iframe src="https://app.banklessacademy.com/lessons/wallet-basics?embed=bankless-africa" width="100%" height="850px"></iframe>
     </div>
   )
 }
