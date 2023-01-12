d082914e2d4460c4d3715e3807fc0809cdc1444d
Atolagbe David Tolulope <atolagbedt@gmail.com>
2023-01-12 19:55:55 +0100

    resize the academy iframe

--- components/Learn-Crypto/Course.js
+++ components/Learn-Crypto/Course.js
@@ -4,13 +4,13 @@ import card2 from "../../asset/Images/Academy_Banner.jpg";
 
 function Course() {
   return (
-    <div className="container mx-auto px-4">
-      <h2 className="text-3xl font-bold py-4">Learn Crypto</h2>
+    <div className="mx-auto bg-black px-23">
+      {/* <h2 className="text-3xl font-bold py-4">Learn Crypto</h2> */}
       {/* <a href="https://app.banklessacademy.com/" target='_blank'>
         <Card title="Bankless Academy" description="Bankless Academy is on a mission to introduce 1 billion people to the exciting possibilities of cryptocurrency, DeFi, and beyond." url={card2} /></a> */}
       {/* <iframe src="https://app.banklessacademy.com/lessons/wallet-basics?embed=bankless-africa" width="100%" height="850px"></iframe> */}
       {/* iframe tyo embed the whole of bankless africa on the learn crypto page */}
-      <iframe src="https://app.banklessacademy.com/?fullembed=bankless-africa" frameborder="0" width='100%' height='850px' ></iframe>
+      <iframe src="https://app.banklessacademy.com/?fullembed=bankless-africa" frameborder="0" width='100%' height='1500px' ></iframe>
     </div>
   )
 }
--- pages/learn-crypto.js
+++ pages/learn-crypto.js
@@ -1,21 +1,23 @@
-import Head from 'next/head'
-import Navbar from '../components/Nav/Navbar'
-import Footer from '../components/Footer/Footer'
-import Newsletter from '../components/Home/Newsletter'
-import Course from '../components/Learn-Crypto/Course'
+import Head from 'next/head';
+import Navbar from '../components/Nav/Navbar';
+import Footer from '../components/Footer/Footer';
+import Newsletter from '../components/Home/Newsletter';
+import Course from '../components/Learn-Crypto/Course';
 
 const Learn = () => {
-    return(
-        <div>
-            <Head>
-            <title>Learn Crypto | Bankless Africa</title>
-            <link rel='icon' href='/favicon.ico' />
-            </Head>
-            <Navbar component="learn"/>
-            <Course />
-            <Newsletter />
-            <Footer />
-        </div>
-    );
-}
+  return (
+    <div>
+      <Head>
+        <title>Learn Crypto | Bankless Africa</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <Navbar component="learn" />
+      <div className="">
+        <Course />
+        <Footer />
+      </div>
+    </div>
+  );
+};
 export default Learn;
