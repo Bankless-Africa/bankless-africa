c9dd31ef01338a15aee804d7acd98793fb0d1472
Atolagbe David Tolulope <atolagbedt@gmail.com>
2023-01-26 00:10:37 +0100

    added academy-collab image

--- components/Home/Card.js
+++ components/Home/Card.js
@@ -1,19 +1,27 @@
-import React, { useEffect } from "react";
-import Image from "next/image";
+import React, { useEffect } from 'react';
+import Image from 'next/image';
 
 function Card({ page, title, description, url }) {
-
   return (
-    <div className="w-64 shadow-2xl bg-gradient-to-b from-zinc-800 to-zinc-800 rounded-xl xl:w-96">
-      <a href={page}>
-      <div className=""><Image src={url} layout="responsive" width={200} height={150} alt=""/></div>
-      <div className="px-4 pb-2">
-        <h2 className="mt-2 text-lg font-normal text-white">{title}</h2>
-        <p className="text-base font-light text-white">{description}</p>
+    <div className="w-64 shadow-2xl bg-gradient-to-b rounded-t-2xl rounded-b-xl from-zinc-800 to-zinc-800 xl:w-96">
+        <a href={page} className='rounded-lg'>
+          <div className="">
+            <Image
+              src={url}
+              layout="responsive"
+              width={200}
+              height={150}
+              alt=""
+              className='rounded-t-3xl'
+            />
+          </div> 
+          <div className="px-4 pb-2">
+            <h2 className="mt-2 text-lg font-normal text-white">{title}</h2>
+            <p className="text-base font-light text-white">{description}</p>
+          </div>
+        </a>
       </div>
-      </a>
-    </div>
-  )
+  );
 }
 
-export default Card
+export default Card;
