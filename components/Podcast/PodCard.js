476477e79503a3b6f0bdca855467f91a04d7fe30
BOBO <114884517+BOBOtoTHEmoon@users.noreply.github.com>
2023-03-08 09:18:43 -0800

    Update PodCard.js
    Install this react-slick library:
    npm install react-slick slick-carousel

--- components/Podcast/PodCard.js
+++ components/Podcast/PodCard.js
@@ -6,6 +6,10 @@ import Image from "next/image";
 import { ShareIcon, SpeakerWaveIcon, XMarkIcon  } from "@heroicons/react/24/outline";
 import { FaSpotify, FaApple, FaRss, FaGoogle, FaWifi } from "react-icons/fa";
 import { Link } from "react-scroll";
+import Slider from 'react-slick';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+
 
 const PodCard = () => {
   const cancelButtonRef = useRef(null);
@@ -15,95 +19,105 @@ const PodCard = () => {
   const [Readmore, setReadmore] = React.useState(false)
 
   return (
-    <div>
-  
-      <section class="cardbg max-w-6xl px-3 py-8 sm:px-20 md:px-32 lg:px-16 mx-auto relative rounded-3xl">
-        <div class="container items-center">
-          <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-2 gap-10 my-12 sm:my-16">
-            <div class="flex flex-col">
-            <div className="aspect-w-3 aspect-h-2 sm:aspect-w-3 sm:aspect-h-4">
-                      <Image className="rounded-lg object-cover shadow-lg" src={box1} alt="podcast"  /></div>
-            </div>
-            <div class="flex flex-col">
-              <h2 className="text-3xl font-bold text-white">
-                Pidgin Parlour Crypto Podcast
-                <br />
-              </h2>
-              <p className="text-xl mt-3 mb-3 text-white">by Pidgin Parlour</p>
+    <div class="container mx-auto max-w-6xl">
+      <Slider
+        dots={true}
+        infinite={true}
+        speed={500}
+        slidesToShow={1}
+        slidesToScroll={1}
+        autoplay={true}
+        autoplaySpeed={4000}
+      >
+          <section class="cardbg max-w-6xl px-3 py-8 sm:px-20 md:px-32 lg:px-16 mx-auto relative rounded-3xl ">
+          <div class="container items-center">
+            <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-2 gap-10 my-12 sm:my-16">
+              <div class="flex flex-col">
+              <div className="aspect-w-3 aspect-h-2 sm:aspect-w-3 sm:aspect-h-4">
+                        <Image className="rounded-lg object-cover shadow-lg" src={box1} alt="podcast"  /></div>
+              </div>
+              <div class="flex flex-col">
+                <h2 className="text-3xl font-bold text-white">
+                  Pidgin Parlour Crypto Podcast
+                  <br />
+                </h2>
+                <p className="text-xl mt-3 mb-3 text-white">by Pidgin Parlour</p>
 
-              <p class="text-gray-200 text-base sm:text-1xl leading-6 lg:text-1xl break-all">
-                Pidgin Parlor na centre of crypto education and culture for the
-                African pipul wey dey speak Pidgin. The show go dey break down
-                plenti crypto concepts wey just dey confuse person into simple
-                forms wey everyday African go fit dey relate with. And all these
-                things go dey for pidgin, the localized language wey over 75
-                million Africans dey speak. This pidgin parlour crypto podcast
-                dey bring crypto education and culture come inside s...
-                <span className="text-red-600">
-                <Link onClick={() => setReadmore(!Readmore)}> Read more </Link>
-                </span>
-              </p>
+                <p class="text-gray-200 text-base sm:text-1xl leading-6 lg:text-1xl break-all">
+                  Pidgin Parlor na centre of crypto education and culture for the
+                  African pipul wey dey speak Pidgin. The show go dey break down
+                  plenti crypto concepts wey just dey confuse person into simple
+                  forms wey everyday African go fit dey relate with. And all these
+                  things go dey for pidgin, the localized language wey over 75
+                  million Africans dey speak. This pidgin parlour crypto podcast
+                  dey bring crypto education and culture come inside s...
+                  <span className="text-red-600">
+                  <Link onClick={() => setReadmore(!Readmore)}> Read more </Link>
+                  </span>
+                </p>
 
-              <div className="flex space-x-4 mt-10">
-                <button onClick={() => setOpen(!Open)} className="flex items-center p-3 text-white border-2 border-white rounded-lg">
-                  <span>
-                    <SpeakerWaveIcon className="h-6 w-6 text-white" />
-                  </span>
-                  <p className="ml-2"> Listen on...</p>
-                </button>
-                <button onClick={() => setOpen(!Open)} className="flex items-center p-3 text-white border-2 border-white rounded-lg">
-                  <span>
-                    <ShareIcon className="h-6 w-6 text-white" />
-                  </span>
-                  <p className="ml-2"> Share</p>
-                </button>
+                <div className="flex space-x-4 mt-10">
+                  <button onClick={() => setOpen(!Open)} className="flex items-center p-3 text-white border-2 border-white rounded-lg">
+                    <span>
+                      <SpeakerWaveIcon className="h-6 w-6 text-white" />
+                    </span>
+                    <p className="ml-2"> Listen on...</p>
+                  </button>
+                  <button onClick={() => setOpen(!Open)} className="flex items-center p-3 text-white border-2 border-white rounded-lg">
+                    <span>
+                      <ShareIcon className="h-6 w-6 text-white" />
+                    </span>
+                    <p className="ml-2"> Share</p>
+                  </button>
+                </div>
               </div>
             </div>
           </div>
-        </div>
-      </section>
+        </section>
 
-      <section class="cardbg mt-10 max-w-6xl px-3 py-8 sm:px-20 md:px-32 lg:px-16 mx-auto relative rounded-3xl">
-        <div class="container items-center">
-          <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-2 gap-10 my-12 sm:my-16">
-            <div class="flex flex-col">
-            <div className="aspect-w-3 aspect-h-2 sm:aspect-w-3 sm:aspect-h-4">
-                      <Image className="rounded-lg object-cover shadow-lg" src={box2} alt="podcast"  /></div>
-            </div>
-            <div class="flex flex-col">
-              <h2 className="text-3xl font-bold text-white">
-                Sats and Gwei Podcast
-                <br />
-              </h2>
-              <p className="text-xl mt-3 mb-3 text-white">by Sats and Gwei</p>
+        <section class="cardbg mt-10 max-w-6xl px-3 py-8 sm:px-20 md:px-32 lg:px-16 mx-auto relative rounded-3xl">
+          <div class="container items-center">
+            <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-2 gap-10 my-12 sm:my-16">
+              <div class="flex flex-col">
+              <div className="aspect-w-3 aspect-h-2 sm:aspect-w-3 sm:aspect-h-4">
+                        <Image className="rounded-lg object-cover shadow-lg" src={box2} alt="podcast"  /></div>
+              </div>
+              <div class="flex flex-col">
+                <h2 className="text-3xl font-bold text-white">
+                  Sats and Gwei Podcast
+                  <br />
+                </h2>
+                <p className="text-xl mt-3 mb-3 text-white">by Sats and Gwei</p>
 
-              <p class="text-gray-200 text-base sm:text-1xl leading-6 lg:text-1xl break-all">
-                Bankless Africa presents Sats & Gwei, a podcast for your weekly
-                crypto news update from all around Africa!
-                <br />
-                Sats & Gwei is a Bankless Africa podcast that brings you weekly
-                crypto/web3 headlines about the Africa continent. Stay up to
-                date with the most relevant crypto happenings across Africa.
-              </p>
+                <p class="text-gray-200 text-base sm:text-1xl leading-6 lg:text-1xl break-all">
+                  Bankless Africa presents Sats & Gwei, a podcast for your weekly
+                  crypto news update from all around Africa!
+                  <br />
+                  Sats & Gwei is a Bankless Africa podcast that brings you weekly
+                  crypto/web3 headlines about the Africa continent. Stay up to
+                  date with the most relevant crypto happenings across Africa.
+                </p>
 
-              <div className="flex space-x-4 mt-10">
-                <button onClick={() => setOpen(!Open)} className="flex items-center p-3 text-white border-2 border-white rounded-lg">
-                  <span>
-                    <SpeakerWaveIcon className="h-6 w-6 text-white" />
-                  </span>
-                  <p className="ml-2"> Listen on...</p>
-                </button>
-                <button onClick={() => setOpen(!Open)} className="flex items-center p-3 text-white border-2 border-white rounded-lg">
-                  <span>
-                    <ShareIcon className="h-6 w-6 text-white" />
-                  </span>
-                  <p className="ml-2"> Share</p>
-                </button>
+                <div className="flex space-x-4 mt-10">
+                  <button onClick={() => setOpen(!Open)} className="flex items-center p-3 text-white border-2 border-white rounded-lg">
+                    <span>
+                      <SpeakerWaveIcon className="h-6 w-6 text-white" />
+                    </span>
+                    <p className="ml-2"> Listen on...</p>
+                  </button>
+                  <button onClick={() => setOpen(!Open)} className="flex items-center p-3 text-white border-2 border-white rounded-lg">
+                    <span>
+                      <ShareIcon className="h-6 w-6 text-white" />
+                    </span>
+                    <p className="ml-2"> Share</p>
+                  </button>
+                </div>
               </div>
             </div>
           </div>
-        </div>
-      </section>
+        </section>
+      </Slider>
+
       <Transition.Root show={Open} as={Fragment}>
       <Dialog as="div" className="relative z-10" initialFocus={cancelButtonRef} onClose={setOpen}>
         <Transition.Child
@@ -161,25 +175,25 @@ const PodCard = () => {
                     
                   <FaApple className="h-6 w-6 text-fuchsia-500" />
                   </span>
-                  <p className="text-sm ml-2"> apple Podcast</p>
+                  <a href="https://podcasts.apple.com/us/podcast/pidgin-parlour-crypto-podcast/id1651579729" className="text-sm ml-2"> apple Podcast</a>
                 </button>
                 <button className="flex items-center p-1 text-white border-2 border-white rounded-lg">
                   <span>
                   <FaRss className="h-6 w-6 text-gray-500" />
                   </span>
-                  <p className="text-sm ml-2"> Rss Podcast</p>
+                  <a href="https://rss.com/podcasts/pidginparlour/" className="text-sm ml-2"> Rss Podcast</a>
                 </button>
                 <button  className="flex items-center p-1 text-white border-2 border-white rounded-lg">
                   <span>
                   <FaWifi className="h-6 w-6 text-green-900" />
                   </span>
-                  <p className="text-sm ml-2"> Podbean</p>
+                  <a href="https://www.podbean.com/podcast-detail/4xqnz-28b6b5/Pidgin-Parlour-Crypto-Podcast" className="text-sm ml-2"> Podbean</a>
                 </button>
                 <button className="flex items-center p-1 text-white border-2 border-white rounded-lg">
                   <span>
                   <FaSpotify className="h-6 w-6 text-green-500" />
                   </span>
-                  <p className="ml-2"> Spotify</p>
+                  <a href="https://open.spotify.com/show/3hDjVgNS0pj0eJY7V9Q2wf" className="ml-2"> Spotify</a>
                 </button>
               
               </div>
@@ -188,7 +202,7 @@ const PodCard = () => {
                   <span>
                   <FaGoogle className="h-6 w-6 text-blue-500" />
                   </span>
-                  <p className="text-sm ml-2"> Google podcast</p>
+                  <a href="https://podcasts.google.com/feed/aHR0cHM6Ly9tZWRpYS5yc3MuY29tL3BpZGdpbnBhcmxvdXIvZmVlZC54bWw" className="text-sm ml-2"> Google podcast</a>
                 </button>
                 
               
